79cc3919dfe8ab275ddb7dde5be3377917c94336
Kate <k.sharunenko@gmail.com>
2018-01-31 11:43:48 -0500

    completed exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName =prompt("What is Your First Name?");
+var lastName =prompt("What is Your Last Name?");
+var age =prompt("What is Your Age?");
+
+alert("Your name is " + firstName + " " + lastName + " and you are " + age + " years old.");
+
+console.log("Your name is " + firstName + " " + lastName + ".");
+console.log("You are " + age + " years old.")
